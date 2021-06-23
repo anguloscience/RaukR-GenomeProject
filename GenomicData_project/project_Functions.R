b5a9efe6d57392a02144b62c64de499c9f8fa4fd
Zhuang Liu <55880584+Zhuang-Bio@users.noreply.github.com>
2021-06-23 13:32:16 +0200

    Add files via upload

---
+++ GenomicData_project/project_Functions.R
@@ -0,0 +1,17 @@
+# This function is to separate the "specific" info from the column
+info_extract <- function(data = NULL, info = NULL){
+  #split the string
+  tmp_a <- data %>% str_split(., pattern = ";") %>% unlist()
+  tmp_b <- lapply(tmp_a, FUN = function(x){
+    return(x %>% str_split(., pattern = "=") %>% unlist())
+  })
+  tmp_c <- do.call('rbind', tmp_b) %>% t() %>% as.data.frame()
+  colnames(tmp_c) <- tmp_c[1,]
+  tmp_c <- tmp_c[-1,]
+  return(tmp_c[info])
+}
+# Usage example
+#info_extract(data = chr22vcf$info[15], info = "VT")
+
+
+
